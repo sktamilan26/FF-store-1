8f8080a966dfdb19fe0a0be216c691a1a052ec19
Sasikarthi <sasikarthi248@gmail.com>
2025-04-29 07:44:32 +0530

    Add new
    Add new

---
+++ script.js
@@ -0,0 +1,6 @@
+function buyFile(fileName) {
+    if (confirm("You selected: " + fileName + "\nClick OK to contact us on Instagram!")) {
+        window.location.href = "https://www.instagram.com/sk_8220_?igsh=MTVlNjlycmJ4NGtyYQ==";
+    }
+}
+
